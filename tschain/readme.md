f03e85064c5e0204d9286ee5b44e2227bd379195
reemrev21 <reemrev21@gmail.com>
2022-06-12 23:53:53 +0900

    [add] types/node, made a block

---
+++ tschain/readme.md
@@ -0,0 +1,5 @@
+### 라이브러리 사용 시 타입 정의가 하나도 되어있지 않을때
+
+- DefinitelyTyped
+- 내가 사용하고 싶은 라이브러리의 타입을 install 하고 싶다면?
+  - <code>npm i -D @types/[라이브러리명]</code>
---
+++ tschain/src/index.ts
@@ -0,0 +1,21 @@
+import crypto from "crypto";
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}
